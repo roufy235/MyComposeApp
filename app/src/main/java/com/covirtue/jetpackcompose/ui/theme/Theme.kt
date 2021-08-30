9a306d77f572c2b08ab63d31094622a92ebededd
roufy235 <roufy235@gmail.com>
2021-08-30 14:50:53 +0100

    Dependenciest and color set-up

--- app/src/main/java/com/covirtue/jetpackcompose/ui/theme/Theme.kt
+++ app/src/main/java/com/covirtue/jetpackcompose/ui/theme/Theme.kt
@@ -7,15 +7,19 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = primaryNight,
+    primaryVariant = textNight,
+    secondary = Teal200,
+    onSurface = cardNight,
+    background = backgroundNight
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = primary,
+    primaryVariant = text,
+    secondary = Teal200,
+    onSurface = card,
+    background = background
 
     /* Other default colors to override
     background = Color.White,
